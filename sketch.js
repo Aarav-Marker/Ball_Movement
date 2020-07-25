d89397b2c2320fe9fab8f02a44ff2e6b11f729b6
Aarav Marker <64842913+Aarav-Marker@users.noreply.github.com>
2020-07-25 16:03:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var hypball,database,position;
+
+function setup(){
+
+    database=firebase.database();
+    createCanvas(500,500);
+    hypball = createSprite(250,250,10,10);
+    hypball.shapeColor = "red";
+   var hypballposition=database.ref('ball/position')
+   hypballposition.on("value",readposition,showerror)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+       writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+    // ball.x = ball.x + x;
+    // ball.y = ball.y + y;
+}
+
+function readposition(data){
+    position=data.val();
+    hypball.x=position.x;
+    hypball.y=position.y;
+}
+
+function showerror(){
+    console.log("error");
+}
